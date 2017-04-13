36585b09a0e0d6a47d716ca35c51929be5414a91
Luca Guarro <luca.guarro@gmail.com>
2017-04-12 22:35:11 -0700

    left click on carousel good

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ $('#right-click-area').click(function(){
     $('#peekCarousel > img').eq(next).show().siblings('img').hide();
     console.log($('#peekCarousel > img:visible'));
 });
+
+$('#left-click-area').click(function(){
+    var current = $('#peekCarousel > img:visible').index();
+    var last_index = $('#peekCarousel > img:last').index();
+    var first_index = 0;
+    console.log(current);
+    if(current == first_index){
+        var prev = last_index;    
+    }
+    else{
+        var prev = current-1;
+    }
+    $('#peekCarousel > img').eq(prev).show().siblings('img').hide();
+    console.log($('#peekCarousel > img:visible'));
+});
